27ed1059e97d6a3127c694d7876d301931a1f8e2
Shuffled720 <shuffled720@gmail.com>
2025-12-24 00:22:38 +0530

    updates the api error to response entity

--- src/main/java/com/vishal/learning/advice/GlobalExceptionHandler.java
+++ src/main/java/com/vishal/learning/advice/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.vishal.learning.advice;
 
 import com.vishal.learning.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,13 +10,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ApiError handleResourceNotFoundException(ResourceNotFoundException exception){
-        return new ApiError(exception.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException exception){
+        ApiError error = new ApiError(exception.getMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ApiError handleRuntimeException(RuntimeException exception){
-        return new ApiError(exception.getMessage(), HttpStatus.FORBIDDEN);
+    public ResponseEntity<ApiError> handleRuntimeException(RuntimeException exception){
+        ApiError error = new ApiError(exception.getMessage(), HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(error,HttpStatus.FORBIDDEN);
     }
 
 
